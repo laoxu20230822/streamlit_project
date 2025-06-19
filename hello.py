c38395fb87827ca914d6e37d304667d49594d5b2
xuminghui <xuminghui18610740807@gmail.com>
2025-06-19 11:03:49 +0800

    init

---
+++ hello.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+
+st.write("Hello world")
+
+st.write("小组件")
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+button=st.button('aaaa')
+st.text_input("Your name", key="name")
+st.session_state.name
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+st.write("交互式表格")
+df      #等价于 st.dataframe(df)
+
+st.write("静态表格")
+st.table(df)
+
+import numpy as np
+import pandas as pd
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(10):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
+@st.cache_data
+def long_running_function(param1, param2):
+    return param1 + param2
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from stremalit!")
+
+
+if __name__ == "__main__":
+    main()
