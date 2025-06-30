c474beab538297305911fc97ee50f4c2f001d0d5
xuminghui <xuminghui18610740807@gmail.com>
2025-06-30 15:09:50 +0800

    init

--- view/display_standard_structure.py
+++ view/display_standard_structure.py
@@ -1,3 +1,4 @@
+from operator import index
 import streamlit as st
 import pandas as pd
 from database.standard_structure import  init_standard_structure_db
@@ -5,4 +6,9 @@ from database.standard_structure import  init_standard_structure_db
 def display_standard_structure(standard_code:str):
     standard_structure=init_standard_structure_db()
     detail_for_markdown=standard_structure.detail_to_markdown(standard_code)
+
+    if '术语和定义' in detail_for_markdown:
+        for line in detail_for_markdown.splitlines():
+             print(line)
+        detail_for_markdown='\n'.join([line for line in detail_for_markdown.splitlines() if '3.' not in  line])
     st.markdown(detail_for_markdown,unsafe_allow_html=True)
