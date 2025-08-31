04014be6ac3144b304ab8cc3ea2f15bc0d86329f
xuminghui <xuminghui18610740807@gmail.com>
2025-08-31 20:02:01 +0800

    update

--- view/display_method_query_list.py
+++ view/display_method_query_list.py
@@ -5,7 +5,7 @@ from database.metric import  init_metric_db
 from database.standard_index import StandardIndex
 from database.standard_db import Pageable, StandardDB
 from database.standard_db import WhereCause
-from st_aggrid import AgGrid
+from st_aggrid import AgGrid,GridOptionsBuilder
 
 from database.standard_structure import StandardStructure
 
@@ -13,50 +13,50 @@ from database.standard_structure import StandardStructure
 
 
 
-def display_grid(data:list[dict]):
-    df=pd.DataFrame(data if data else [],columns={
-        'standard_code': '标准号',
-        'standard_name': '标准名称',
-        'standard_content': '标准内容',
-    })
+# def display_grid(data:list[dict]):
+#     df=pd.DataFrame(data if data else [],columns={
+#         'standard_code': '标准号',
+#         'standard_name': '标准名称',
+#         'standard_content': '标准内容',
+#     })
 
-    grid_options = {
-        'suppressNoRowsOverlay': True,
-        'columnDefs': [
-        { 'field': "standard_code", 'headerName': "标准号"},
-        { 'field': "standard_name", 'headerName': "标准名称"},
-        { 'field': "standard_content", 'headerName': "标准内容"},
-    ],
-    'rowSelection': {
-            'mode': 'singleRow',
-            'checkboxes': False,
-            'enableClickSelection': True
-        },
-        "autoSizeStrategy": {
-            "type": "fitCellContents"
-        },
-        "pagination": True,
-        #"paginationAutoPageSize": True,
-        "paginationPageSize": 50
-    }
-    grid_response = AgGrid(
-        df, 
-        gridOptions=grid_options,
-        height=300,
-        #key='asdjflasdjkfl'
-        )
-    selected_rows=grid_response['selected_rows']
+#     grid_options = {
+#         'suppressNoRowsOverlay': True,
+#         'columnDefs': [
+#         { 'field': "standard_code", 'headerName': "标准号"},
+#         { 'field': "standard_name", 'headerName': "标准名称"},
+#         { 'field': "standard_content", 'headerName': "标准内容"},
+#     ],
+#     'rowSelection': {
+#             'mode': 'singleRow',
+#             'checkboxes': False,
+#             'enableClickSelection': True
+#         },
+#         "autoSizeStrategy": {
+#             "type": "fitCellContents"
+#         },
+#         "pagination": True,
+#         #"paginationAutoPageSize": True,
+#         "paginationPageSize": 50
+#     }
+#     grid_response = AgGrid(
+#         df, 
+#         gridOptions=grid_options,
+#         height=300,
+#         #key='asdjflasdjkfl'
+#         )
+#     selected_rows=grid_response['selected_rows']
     
-    if selected_rows is not None:
-        standard_content= [row['standard_content'] for _, row in selected_rows.iterrows()][0]
-        st.container(border=True).markdown(standard_content)
-        st.session_state.selected_rows=[{'standard_code':row['standard_code'],'standard_name':row['standard_name']} for _, row in selected_rows.iterrows()]
-    #     #st.write(test)       # standard_code=row['standard_code']
-    #         # standard_name=row['standard_name']
-    #     if grid_response['selected_rows'] is not None:
-    #             for index,row_chapter in grid_response['selected_rows'].iterrows():
-    #                 st.container(border=True).markdown(row_chapter['standard_content'])
-    return grid_response
+#     if selected_rows is not None:
+#         standard_content= [row['standard_content'] for _, row in selected_rows.iterrows()][0]
+#         st.container(border=True).markdown(standard_content)
+#         st.session_state.selected_rows=[{'standard_code':row['standard_code'],'standard_name':row['standard_name']} for _, row in selected_rows.iterrows()]
+#     #     #st.write(test)       # standard_code=row['standard_code']
+#     #         # standard_name=row['standard_name']
+#     #     if grid_response['selected_rows'] is not None:
+#     #             for index,row_chapter in grid_response['selected_rows'].iterrows():
+#     #                 st.container(border=True).markdown(row_chapter['standard_content'])
+#     return grid_response
 
 # def display_method_query_list(search_term:str):
 #     metric=init_metric_db()
@@ -89,6 +89,8 @@ def display_method_query_list_new(search_term:str):
         .reset_index()
     )
 
+    
+    
     grid_options = {
             'suppressNoRowsOverlay': True,
             'columnDefs': [
@@ -123,6 +125,9 @@ def display_method_query_list_new(search_term:str):
         #"paginationAutoPageSize": True,
         "paginationPageSize": 50
         }
+    # gb_options=GridOptionsBuilder.build()
+    # gb_options.update(grid_options)
+    
     grid_response = AgGrid(
             df_pivot, 
             gridOptions=grid_options,
