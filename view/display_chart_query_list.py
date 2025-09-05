df0826a4ae2055a94244a355553992449e22e6df
xuminghui <xuminghui18610740807@gmail.com>
2025-09-05 15:58:12 +0800

    update

--- view/display_chart_query_list.py
+++ view/display_chart_query_list.py
@@ -16,6 +16,7 @@ from view.display_product_standard import display_product_standard
 from view.display_craft_standard import display_craft_standard
 from view.display_standard_structure import display_standard_structure
 from view.display_standard_tab_info import display_standard_tab_info
+import urllib.parse
 
 imageRenderer = JsCode("""
     class ImageRenderer {
@@ -34,7 +35,7 @@ imageRenderer = JsCode("""
         }
 """)
 
-def show_grid(data):
+def show_grid(data,key:str):
     df=pd.DataFrame(data if data else [],columns={
             'standard_code': '标准号',
             'standard_name': '标准名称',
@@ -47,17 +48,19 @@ def show_grid(data):
     
     #使用下划线
     #df["image_file_name"]="Q_SY 01017-2018岩石物理分析成果数据存储文件结构"
+    df["image_info_path"] = "app/static/" + df["image_file_name"].str.replace("/", "_") + ".png"
     df["image_info"] = "/app/static/" + df["image_file_name"].str.replace("/", "_") + ".png"
     grid_options = {
         "suppressNoRowsOverlay": True,
         'columnDefs': [
-        { 'field': "seq", 'headerName': "序号",'width':40},
-        { 'field': "standard_code", 'headerName': "标准号",'hide':True},
+        { 'field': "seq", 'headerName': "序号",'width':40,'suppressSizeToFit':True},
+        { 'field': "standard_code", 'headerName': "标准号",'hide':True,},
         { 'field': "standard_name", 'headerName': "标准名称",'hide':True},
         { 'field': "in_text_number", 'headerName': "文中编号",'width':100,'hide':True},
-        { 'field': "in_text_name", 'headerName': "图片名称",'width':200},
+        { 'field': "in_text_name", 'headerName': "图片名称",'width':250,'suppressSizeToFit':True},
+        { 'field': "image_info_path", 'headerName': "图片路径",'hide':True},
         { 'field': "image_info", 'headerName': "图片",'cellRenderer': imageRenderer,'width':100,'autoHeight':True},
-        { 'field': "standard_info", 'headerName': "标准来源"},
+        { 'field': "standard_info", 'headerName': "标准来源",'width':400},
     ],
     'rowSelection': {
             'mode': 'singleRow',
@@ -75,9 +78,9 @@ def show_grid(data):
         df, 
         gridOptions=grid_options,
         height=300,
-        # key=str(uuid.uuid4(),
+        key=key,
         fit_columns_on_grid_load=True,
-        allow_unsafe_jscode=True
+        allow_unsafe_jscode=True,
         )
 
 
@@ -92,13 +95,24 @@ def display_chart_query_list(search_term:str):
     standard_chart=init_standard_chart_db()
     with t1:
         data=standard_chart.list_all('图片',search_term)
-        grid_response=show_grid(data)
+        grid_response=show_grid(data,'image')
+        selected_rows=grid_response['selected_rows']
+        if selected_rows is not None:
+            st.session_state.selected_rows=[{'image_info_path':row['image_info_path']} for _, row in selected_rows.iterrows()]
     with t2:
         data=standard_chart.list_all('表格',search_term)
-        grid_response=show_grid(data)
+        grid_response=show_grid(data,'table')
+        selected_rows=grid_response['selected_rows']
+        if selected_rows is not None:
+            st.session_state.selected_rows=[{'image_info_path':row['image_info_path']} for _, row in selected_rows.iterrows()]
     with t3:
         data=standard_chart.list_all('公式',search_term)
-        grid_response=show_grid(data)
+        grid_response=show_grid(data,'formula')
+        selected_rows=grid_response['selected_rows']
+        if selected_rows is not None:
+            st.session_state.selected_rows=[{'image_info_path':row['image_info_path']} for _, row in selected_rows.iterrows()]
+    show_chart()
+    
     # df=pd.DataFrame(data if data else [],columns={
     #         'standard_code': '标准号',
     #         'standard_name': '标准名称',
@@ -141,3 +155,13 @@ def display_chart_query_list(search_term:str):
     # selected_rows=grid_response['selected_rows']
     # if selected_rows is not None:
     #     st.session_state.selected_rows=[{'standard_code':row['standard_code'],'standard_name':row['standard_name']} for _, row in selected_rows.iterrows()]
+
+def show_chart():
+    if "selected_rows" in st.session_state:
+        image_info_path = st.session_state["selected_rows"][0]["image_info_path"]
+        show_image_detail(image_info_path)
+
+@st.dialog("图片详情",width="large")
+def show_image_detail(image_info_path:str):
+    encoded_path = urllib.parse.quote(image_info_path)
+    st.markdown(f"![Selected Image]({encoded_path})",unsafe_allow_html=True)
